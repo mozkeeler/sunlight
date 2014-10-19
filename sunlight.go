14734937eb6d49cb3367ff1337dd10e0bdeaf39e
Monica Chew <mmc@mozilla.com>
2014-10-18 17:46:11 -0700

    Cleanup

--- sunlight.go
+++ sunlight.go
@@ -13,27 +13,32 @@ import (
 	"time"
 )
 
+const (
+	VALID_PERIOD_TOO_LONG = "ValidPeriodTooLong"
+)
+
 // Only fields that start with capital letters are exported
 type CertSummary struct {
-	CN                           string
-	Issuer                       string
-	Sha256Fingerprint            string
-	NotBefore                    string
-	NotAfter                     string
-	ValidPeriodTooLong           bool
-	DeprecatedSignatureAlgorithm bool
-	DeprecatedVersion            bool
-	MissingCNinSAN               bool
-	KeyTooShort                  bool
-	KeySize                      int
-	ExpTooSmall                  bool
-	Exp                          int
-	SignatureAlgorithm           int
-	Version                      int
-	IsCA                         bool
-	DnsNames                     []string
-	IpAddresses                  []string
-	MaxReputation                float32
+	CN                string
+	Issuer            string
+	Sha256Fingerprint string
+	NotBefore         string
+	NotAfter          string
+	Violations        map[string]bool
+	//ValidPeriodTooLong           bool
+	//DeprecatedSignatureAlgorithm bool
+	//DeprecatedVersion            bool
+	//MissingCNinSAN               bool
+	//KeyTooShort                  bool
+	//ExpTooSmall                  bool
+	KeySize            int
+	Exp                int
+	SignatureAlgorithm int
+	Version            int
+	IsCA               bool
+	DnsNames           []string
+	IpAddresses        []string
+	MaxReputation      float32
 }
 
 type IssuerReputationScore struct {
@@ -42,14 +47,15 @@ type IssuerReputationScore struct {
 }
 
 type IssuerReputation struct {
-	Issuer                       string
-	ValidPeriodTooLong           IssuerReputationScore
-	DeprecatedVersion            IssuerReputationScore
-	DeprecatedSignatureAlgorithm IssuerReputationScore
-	MissingCNinSAN               IssuerReputationScore
-	KeyTooShort                  IssuerReputationScore
-	ExpTooSmall                  IssuerReputationScore
-	IsCA                         uint64
+	Issuer string
+	Scores map[string]IssuerReputationScore
+	//ValidPeriodTooLong           IssuerReputationScore
+	//DeprecatedVersion            IssuerReputationScore
+	//DeprecatedSignatureAlgorithm IssuerReputationScore
+	//MissingCNinSAN               IssuerReputationScore
+	//KeyTooShort                  IssuerReputationScore
+	//ExpTooSmall                  IssuerReputationScore
+	IsCA uint64
 	// Issuer reputation, between [0, 1]. This is only affected by certs that
 	// have MaxReputation != -1
 	NormalizedScore float32
@@ -69,9 +75,12 @@ func TimeToJSONString(t time.Time) string {
 }
 
 func (summary *CertSummary) ViolatesBR() bool {
-	return summary.ValidPeriodTooLong || summary.DeprecatedSignatureAlgorithm ||
-		summary.DeprecatedVersion || summary.MissingCNinSAN ||
-		summary.KeyTooShort || summary.ExpTooSmall
+	for _, val := range summary.Violations {
+		if val {
+			return true
+		}
+	}
+	return false
 }
 
 func NewIssuerReputation(issuer string) *IssuerReputation {
@@ -105,50 +114,27 @@ func (issuer *IssuerReputation) Update(summary *CertSummary) {
 		reputation = 0
 	}
 
-	if summary.ValidPeriodTooLong {
-		issuer.ValidPeriodTooLong.Update(reputation)
+	for name, val := range summary.Violations {
+		if val {
+			issuer.Scores[name].Update(reputation)
+		}
 	}
-	if summary.DeprecatedVersion {
-		issuer.DeprecatedVersion.Update(reputation)
-	}
-	if summary.DeprecatedSignatureAlgorithm {
-		issuer.DeprecatedSignatureAlgorithm.Update(reputation)
-	}
-	if summary.MissingCNinSAN {
-		issuer.MissingCNinSAN.Update(reputation)
-	}
-	if summary.KeyTooShort {
-		issuer.KeyTooShort.Update(reputation)
-	}
-	if summary.ExpTooSmall {
-		issuer.ExpTooSmall.Update(reputation)
-	}
+
 	if summary.IsCA {
 		issuer.IsCA += 1
 	}
 }
 
 func (issuer *IssuerReputation) Finish() {
-	issuer.ValidPeriodTooLong.Finish(issuer.NormalizedCount, issuer.RawCount)
-	issuer.DeprecatedVersion.Finish(issuer.NormalizedCount, issuer.RawCount)
-	issuer.DeprecatedSignatureAlgorithm.Finish(issuer.NormalizedCount, issuer.RawCount)
-	issuer.MissingCNinSAN.Finish(issuer.NormalizedCount, issuer.RawCount)
-	issuer.KeyTooShort.Finish(issuer.NormalizedCount, issuer.RawCount)
-	issuer.ExpTooSmall.Finish(issuer.NormalizedCount, issuer.RawCount)
-
-	// Calculate total score
-	issuer.NormalizedScore = (issuer.ValidPeriodTooLong.NormalizedScore +
-		issuer.DeprecatedVersion.NormalizedScore +
-		issuer.DeprecatedSignatureAlgorithm.NormalizedScore +
-		issuer.MissingCNinSAN.NormalizedScore +
-		issuer.KeyTooShort.NormalizedScore +
-		issuer.ExpTooSmall.NormalizedScore) / 6
-	issuer.RawScore = (issuer.ValidPeriodTooLong.RawScore +
-		issuer.DeprecatedVersion.RawScore +
-		issuer.DeprecatedSignatureAlgorithm.RawScore +
-		issuer.MissingCNinSAN.RawScore +
-		issuer.KeyTooShort.RawScore +
-		issuer.ExpTooSmall.RawScore) / 6
+	normalizedSum := 0.0
+	rawSum := 0.0
+	for _, score := range issuer.Scores {
+		score.Finish(issuer.NormalizedCount, issuer.RawCount)
+		normalizedSum += score.NormalizedScore
+		rawSum != score.RawScore
+	}
+	issuer.NormalizedScore = normalizedSum / len(issuer.Scores)
+	issuer.rawScore = rawSum / len(issuer.Scores)
 }
 
 func CalculateCertSummary(cert *x509.Certificate, ranker *alexa.AlexaRank) (result *CertSummary, err error) {
@@ -163,87 +149,88 @@ func CalculateCertSummary(cert *x509.Certificate, ranker *alexa.AlexaRank) (resu
 
 	// BR 9.4.1: Validity period is longer than 5 years.  This
 	// should be restricted to certs that don't have CA:True
-	summary.ValidPeriodTooLong = false
+	summary.Violations[VALID_PERIOD_TOO_LONG] = false
 
-	if cert.NotAfter.After(cert.NotBefore.AddDate(5, 0, 7)) &&
-		(!cert.BasicConstraintsValid || (cert.BasicConstraintsValid && !cert.IsCA)) {
-		summary.ValidPeriodTooLong = true
-	}
-
-	// SignatureAlgorithm is SHA1
-	summary.DeprecatedSignatureAlgorithm = false
-	if cert.SignatureAlgorithm == x509.SHA1WithRSA ||
-		cert.SignatureAlgorithm == x509.DSAWithSHA1 ||
-		cert.SignatureAlgorithm == x509.ECDSAWithSHA1 {
-		summary.DeprecatedSignatureAlgorithm = true
-	}
-
-	// Uses v1 certificates
-	summary.DeprecatedVersion = cert.Version != 3
-
-	// Public key length <= 1024 bits
-	summary.KeyTooShort = false
-	summary.ExpTooSmall = false
-	summary.KeySize = -1
-	summary.Exp = -1
-	parsedKey, ok := cert.PublicKey.(*rsa.PublicKey)
-	if ok {
-		summary.KeySize = parsedKey.N.BitLen()
-		summary.Exp = parsedKey.E
-		if summary.KeySize <= 1024 {
-			summary.KeyTooShort = true
+	/*
+		if cert.NotAfter.After(cert.NotBefore.AddDate(5, 0, 7)) &&
+			(!cert.BasicConstraintsValid || (cert.BasicConstraintsValid && !cert.IsCA)) {
+			summary.ValidPeriodTooLong = true
 		}
-		if summary.Exp <= 3 {
-			summary.ExpTooSmall = true
+
+		// SignatureAlgorithm is SHA1
+		summary.DeprecatedSignatureAlgorithm = false
+		if cert.SignatureAlgorithm == x509.SHA1WithRSA ||
+			cert.SignatureAlgorithm == x509.DSAWithSHA1 ||
+			cert.SignatureAlgorithm == x509.ECDSAWithSHA1 {
+			summary.DeprecatedSignatureAlgorithm = true
 		}
-	}
 
-	summary.MaxReputation, _ = ranker.GetReputation(cert.Subject.CommonName)
-	for _, host := range cert.DNSNames {
-		reputation, _ := ranker.GetReputation(host)
-		if reputation > summary.MaxReputation {
-			summary.MaxReputation = reputation
-		}
-	}
-	sha256hasher := sha256.New()
-	sha256hasher.Write(cert.Raw)
-	summary.Sha256Fingerprint = base64.StdEncoding.EncodeToString(sha256hasher.Sum(nil))
+		// Uses v1 certificates
+		summary.DeprecatedVersion = cert.Version != 3
 
-	// DNS names and IP addresses
-	summary.DnsNames = cert.DNSNames
-	for _, address := range cert.IPAddresses {
-		summary.IpAddresses = append(summary.IpAddresses, address.String())
-	}
-
-	// Assume a 0-length CN means it isn't present (this isn't a good
-	// assumption). If the CN is missing, then it can't be missing CN in SAN.
-	summary.MissingCNinSAN = false
-	if len(cert.Subject.CommonName) == 0 {
-		return &summary, nil
-	}
-
-	cnAsPunycode, err := idna.ToASCII(cert.Subject.CommonName)
-	if err != nil {
-		return &summary, nil
-	}
-
-	// BR 9.2.2: Found Common Name in Subject Alt Names, either as an IP or a
-	// DNS name.
-	summary.MissingCNinSAN = true
-	cnAsIP := net.ParseIP(cert.Subject.CommonName)
-	if cnAsIP != nil {
-		for _, ip := range cert.IPAddresses {
-			if cnAsIP.Equal(ip) {
-				summary.MissingCNinSAN = false
+		// Public key length <= 1024 bits
+		summary.KeyTooShort = false
+		summary.ExpTooSmall = false
+		summary.KeySize = -1
+		summary.Exp = -1
+		parsedKey, ok := cert.PublicKey.(*rsa.PublicKey)
+		if ok {
+			summary.KeySize = parsedKey.N.BitLen()
+			summary.Exp = parsedKey.E
+			if summary.KeySize <= 1024 {
+				summary.KeyTooShort = true
+			}
+			if summary.Exp <= 3 {
+				summary.ExpTooSmall = true
 			}
 		}
-	} else {
-		for _, san := range cert.DNSNames {
-			if err == nil && strings.EqualFold(san, cnAsPunycode) {
-				summary.MissingCNinSAN = false
+
+		summary.MaxReputation, _ = ranker.GetReputation(cert.Subject.CommonName)
+		for _, host := range cert.DNSNames {
+			reputation, _ := ranker.GetReputation(host)
+			if reputation > summary.MaxReputation {
+				summary.MaxReputation = reputation
 			}
 		}
-	}
+		sha256hasher := sha256.New()
+		sha256hasher.Write(cert.Raw)
+		summary.Sha256Fingerprint = base64.StdEncoding.EncodeToString(sha256hasher.Sum(nil))
 
+		// DNS names and IP addresses
+		summary.DnsNames = cert.DNSNames
+		for _, address := range cert.IPAddresses {
+			summary.IpAddresses = append(summary.IpAddresses, address.String())
+		}
+
+		// Assume a 0-length CN means it isn't present (this isn't a good
+		// assumption). If the CN is missing, then it can't be missing CN in SAN.
+		summary.MissingCNinSAN = false
+		if len(cert.Subject.CommonName) == 0 {
+			return &summary, nil
+		}
+
+		cnAsPunycode, err := idna.ToASCII(cert.Subject.CommonName)
+		if err != nil {
+			return &summary, nil
+		}
+
+		// BR 9.2.2: Found Common Name in Subject Alt Names, either as an IP or a
+		// DNS name.
+		summary.MissingCNinSAN = true
+		cnAsIP := net.ParseIP(cert.Subject.CommonName)
+		if cnAsIP != nil {
+			for _, ip := range cert.IPAddresses {
+				if cnAsIP.Equal(ip) {
+					summary.MissingCNinSAN = false
+				}
+			}
+		} else {
+			for _, san := range cert.DNSNames {
+				if err == nil && strings.EqualFold(san, cnAsPunycode) {
+					summary.MissingCNinSAN = false
+				}
+			}
+		}
+	*/
 	return &summary, nil
 }
--- sunlight_test.go
+++ sunlight_test.go
@@ -1,10 +1,34 @@
 package sunlight
 
-import "testing"
-import "os"
-import "encoding/json"
+import (
+	"encoding/json"
+	"encoding/pem"
+	"os"
+	"testing"
+)
 
-func TestInit(t *testing.T) {
+const pemCertificate = `-----BEGIN CERTIFICATE-----
+MIIB5DCCAZCgAwIBAgIBATALBgkqhkiG9w0BAQUwLTEQMA4GA1UEChMHQWNtZSBDbzEZMBcGA1UE
+AxMQdGVzdC5leGFtcGxlLmNvbTAeFw03MDAxMDEwMDE2NDBaFw03MDAxMDIwMzQ2NDBaMC0xEDAO
+BgNVBAoTB0FjbWUgQ28xGTAXBgNVBAMTEHRlc3QuZXhhbXBsZS5jb20wWjALBgkqhkiG9w0BAQED
+SwAwSAJBALKZD0nEffqM1ACuak0bijtqE2QrI/KLADv7l3kK3ppMyCuLKoF0fd7Ai2KW5ToIwzFo
+fvJcS/STa6HA5gQenRUCAwEAAaOBnjCBmzAOBgNVHQ8BAf8EBAMCAAQwDwYDVR0TAQH/BAUwAwEB
+/zANBgNVHQ4EBgQEAQIDBDAPBgNVHSMECDAGgAQBAgMEMBsGA1UdEQQUMBKCEHRlc3QuZXhhbXBs
+ZS5jb20wDwYDVR0gBAgwBjAEBgIqAzAqBgNVHR4EIzAhoB8wDoIMLmV4YW1wbGUuY29tMA2CC2V4
+YW1wbGUuY29tMAsGCSqGSIb3DQEBBQNBAHKZKoS1wEQOGhgklx4+/yFYQlnqwKXvar/ZecQvJwui
+0seMQnwBhwdBkHfVIU2Fu5VUMRyxlf0ZNaDXcpU581k=
+-----END CERTIFICATE-----`
+
+func TestCertSummary(t *testing.T) {
+	pemBlock, _ := pem.Decode([]byte(pemCertificate)
+	cert, err := ParseCertificate(pemBlock.Bytes)
+  summary := CalculateCertSummary(&cert, nil)
+	if summary.ValidPeriodTooLong {
+		t.Error("Valid period too long\n")
+	}
+}
+
+func TestIssuerReputation(t *testing.T) {
 	summary := CertSummary{
 		CN:                           "example.com",
 		Issuer:                       "Honest Al",
