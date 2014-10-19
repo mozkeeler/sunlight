e4443ecacb4688a3dbc4ee29685187c6533004c7
Monica Chew <mmc@mozilla.com>
2014-10-18 18:42:34 -0700

    Clean up

--- sunlight_test.go
+++ sunlight_test.go
@@ -1,6 +1,7 @@
 package sunlight
 
 import (
+	"crypto/x509"
 	"encoding/json"
 	"encoding/pem"
 	"os"
@@ -20,40 +21,48 @@ YW1wbGUuY29tMAsGCSqGSIb3DQEBBQNBAHKZKoS1wEQOGhgklx4+/yFYQlnqwKXvar/ZecQvJwui
 -----END CERTIFICATE-----`
 
 func TestCertSummary(t *testing.T) {
-	pemBlock, _ := pem.Decode([]byte(pemCertificate)
-	cert, err := ParseCertificate(pemBlock.Bytes)
-  summary := CalculateCertSummary(&cert, nil)
-	if summary.ValidPeriodTooLong {
-		t.Error("Valid period too long\n")
+	pemBlock, _ := pem.Decode([]byte(pemCertificate))
+	cert, _ := x509.ParseCertificate(pemBlock.Bytes)
+	summary, _ := CalculateCertSummary(cert, nil)
+	if summary.Violations[VALID_PERIOD_TOO_LONG] {
+		t.Error("Valid period too long")
 	}
+	if summary.Violations[KEY_TOO_SHORT] || summary.KeySize != 512 {
+		t.Error("Expected key too short")
+	}
+	b, _ := json.MarshalIndent(summary, "", "  ")
+	os.Stderr.Write(b)
 }
 
 func TestIssuerReputation(t *testing.T) {
 	summary := CertSummary{
-		CN:                           "example.com",
-		Issuer:                       "Honest Al",
-		Sha256Fingerprint:            "foo",
-		ValidPeriodTooLong:           true,
-		DeprecatedSignatureAlgorithm: false,
-		DeprecatedVersion:            false,
-		MissingCNinSAN:               true,
-		KeyTooShort:                  false,
-		ExpTooSmall:                  false,
-		IsCA:                         false,
-		MaxReputation:                0.1,
+		CN:                "example.com",
+		Issuer:            "Honest Al",
+		Sha256Fingerprint: "foo",
+		Violations: map[string]bool{
+			VALID_PERIOD_TOO_LONG:          true,
+			DEPRECATED_SIGNATURE_ALGORITHM: false,
+			DEPRECATED_VERSION:             false,
+			KEY_TOO_SHORT:                  false,
+			EXP_TOO_SMALL:                  false,
+			MISSING_CN_IN_SAN:              true,
+		},
+		MaxReputation: 0.1,
 	}
 	unknown_summary := CertSummary{
-		CN:                           "unknown.example.com",
-		Issuer:                       "Honest Al",
-		Sha256Fingerprint:            "foo",
-		ValidPeriodTooLong:           true,
-		DeprecatedSignatureAlgorithm: false,
-		DeprecatedVersion:            false,
-		MissingCNinSAN:               true,
-		KeyTooShort:                  false,
-		ExpTooSmall:                  false,
-		IsCA:                         false,
-		MaxReputation:                -1,
+		CN:                "unknown.example.com",
+		Issuer:            "Honest Al",
+		Sha256Fingerprint: "foo",
+		Violations: map[string]bool{
+			VALID_PERIOD_TOO_LONG:          true,
+			DEPRECATED_SIGNATURE_ALGORITHM: false,
+			DEPRECATED_VERSION:             false,
+			KEY_TOO_SHORT:                  false,
+			EXP_TOO_SMALL:                  false,
+			MISSING_CN_IN_SAN:              true,
+		},
+		IsCA:          false,
+		MaxReputation: -1,
 	}
 	issuer := NewIssuerReputation("Honest Al")
 	issuer.Update(&summary)
@@ -65,7 +74,7 @@ func TestIssuerReputation(t *testing.T) {
 	if issuer.NormalizedCount != 1 {
 		t.Error("Should have normalized count of 1")
 	}
-	if issuer.ValidPeriodTooLong.NormalizedScore != 0.9 {
+	if issuer.Scores[VALID_PERIOD_TOO_LONG].NormalizedScore != 0.9 {
 		t.Error("Should have score of 0.9")
 	}
 	b, _ := json.MarshalIndent(issuer, "", "  ")
