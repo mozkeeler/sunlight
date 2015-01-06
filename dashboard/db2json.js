0ef837f794badf02e0e8a469bcb62954b0bcf8da
Monica Chew <mmc@mozilla.com>
2015-01-06 14:40:11 -0800

    Make timeseries in sql instead of js

--- dashboard/db2json.js
+++ dashboard/db2json.js
@@ -1,12 +1,49 @@
 var sqliteToJSON = require('sqlite-to-json');
 var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('BRs.db');
 
-var exporter = new sqliteToJSON({
-  client: new sqlite3.Database('BRs.db')
-});
+console.log("var timeseries = {};\n");
 
-exporter.save('issuerReputation', 'issuerReputation.json', function(err) {
-  if (err) {
-    console.log('error exporting: ' + err);
-  }
-});
+function printTimeseries(timeseries) {
+  var name = timeseries.name.replace(/[ \.-]/g, "_") + "_Timeseries";
+  console.log("var " + name + " = " + JSON.stringify(timeseries) + ";\n");
+  console.log("timeseries[\"" + timeseries.name + "\"] = " + name + ";\n");
+}
+
+function makeTimeseriesForIssuer(issuer, column, cb) {
+  var timeseries = { name: issuer, data: [] };
+  db.all("SELECT beginTime as t, " + column + " AS d " +
+         "FROM issuerReputation WHERE issuer=\"" + issuer +
+         "\" ORDER BY t", function (err, rows) {
+    for (i = 0; i < rows.length; ++i) {
+      timeseries.data.push([rows[i].t, rows[i].d]);
+    }
+    cb(timeseries);
+  });
+}
+
+function completionDump(name, issuerArray) {
+  console.log("var " + name + " = " + JSON.stringify(issuerArray) + ";\n");
+}
+
+// An array of issuers
+var topIssuers = [];
+var worstIssuers = [];
+db.each("SELECT issuer, sum(rawCount) AS n FROM issuerReputation " +
+        "GROUP BY issuer ORDER BY n DESC LIMIT 10;",
+  function(err, row) {
+    topIssuers.push(row.issuer);
+    makeTimeseriesForIssuer(row.issuer, "rawScore", printTimeseries);
+  },
+  function() { completionDump("topIssuers", topIssuers); });
+
+// We can't restrict the query based on aliases (e.g., SUM(col)) so make a
+// subquery instead.
+db.each("SELECT issuer, n FROM (SELECT issuer, rawScore AS n, " +
+        "SUM(rawCount) AS s FROM issuerReputation GROUP BY issuer) " +
+        "AS NEWTABLE WHERE s > 1000 AND issuer != \"\" ORDER BY n LIMIT 10;",
+  function(err, row) {
+    worstIssuers.push(row.issuer);
+    makeTimeseriesForIssuer(row.issuer, "rawScore", printTimeseries);
+  },
+  function() { completionDump("worstIssuers", worstIssuers); });
